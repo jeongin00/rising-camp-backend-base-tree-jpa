ec2190890f1c92837004a72a849b98b262b0f51e
jeongin00 <hjeongin1001@gmail.com>
2025-08-13 11:53:20 +0900

    Delete repository directory

--- repository/IUserRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.repository.entity.User;
-
-import java.util.List;
-
-public interface IUserRepositoryCustom {
-    List<User> findByName(String name);
-}
--- repository/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.repository.entity.Message;
-import com.example.demo.repository.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-}
